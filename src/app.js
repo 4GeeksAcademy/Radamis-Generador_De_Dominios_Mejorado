0310b987468e0062b9c928cc2b2f20c50f073b96
radamiscunha19-ux <radamis.cunha19@gmail.com>
2025-10-10 18:45:03 +0000

    intentando

--- src/app.js
+++ src/app.js
@@ -5,7 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+window.onload = function () {
+
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let ext = [".com", ".com.br", ".us", ".es", ".net", ".io"];
+
+  let list = "<ul>";
+
+  for(let a=0; a<pronoun.length; a++){
+    for(let b=0; b<adj.length; b++){
+      for(let c=0; c<noun.length; c++){
+        for(let d=0; d<ext.length; d++){
+
+          let resultado = "<li>" + pronoun[a] + adj[b] + noun[c] + ext[d] + "</li>";
+         list = resultado + "</ul>"
+         
+         console.log(resultado);
+
+        
+        }
+      }
+    } 
+  }
+
+document.getElementById("dominio").innerHTML = list;
   console.log("Hello Rigo from the console!");
 };
+
+
+
